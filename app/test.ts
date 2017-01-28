54efcec11d11849fa0b3e9b9294f551d87aefdc0
WireJunky <antoniecoetzee@gmail.com>
2017-01-28 14:35:10 +0200

    getting typescript going

---
+++ app/test.ts
@@ -0,0 +1,6 @@
+class Startup {
+    public static main(): number {
+        console.log('Hello World');
+        return 0;
+    }
+}
---
+++ dist/project.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+var Startup = (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('Hello World');
+        return 0;
+    };
+    return Startup;
+}());
+//# sourceMappingURL=project.js.map
