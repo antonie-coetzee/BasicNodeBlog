2c8e1fab826fa2d8e5504b46094e221cc0285963
WireJunky <antoniecoetzee@gmail.com>
2017-02-02 20:06:17 +0200

    updated tsconfig

--- app/client/test.ts
+++ app/client/test.ts
@@ -1,6 +1,23 @@
-class Startup {
-    public static main(): number {
-        console.log('Hello World');
-        return 0;
+import Vue = require('vue')
+// Declare the component's type
+interface MyComponent extends Vue {
+  message: string
+  onClick (): void
+}
+export default {
+  template: '<button @click="onClick">Click!</button>',
+  data: function () {
+    return {
+      message: 'Hello!'
     }
-} 
+  },
+  methods: {
+    onClick: function () {
+      // TypeScript knows that `this` is of type MyComponent
+      // and that `this.message` will be a string
+      window.alert(this.message)
+    }
+  }
+// We need to explicitly annotate the exported options object
+// with the MyComponent type
+} as Vue.ComponentOptions<MyComponent>
