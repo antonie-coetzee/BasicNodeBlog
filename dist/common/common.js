9df69b6d1abdd8f8191a0bf67a232d2d5a2ffd09
WireJunky <antoniecoetzee@gmail.com>
2017-02-02 19:01:35 +0200

    ignoring dist folder

--- dist/common/common.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var CommonClass = (function () {
-    function CommonClass() {
-    }
-    return CommonClass;
-}());
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = CommonClass;
-//# sourceMappingURL=common.js.map
