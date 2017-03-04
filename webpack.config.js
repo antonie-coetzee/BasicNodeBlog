3391459e968ab4778ed3d772d48f949407e2169a
WireJunky <antoniecoetzee@gmail.com>
2017-03-04 15:29:59 +0200

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,39 @@
 const path = require('path');
 const webpack = require('webpack'); 
 
+const Visualizer = require('webpack-visualizer-plugin');
+
 module.exports = {
-    entry: path.resolve(__dirname, '../client.tsx'),
+    devtool: 'source-map',
+    entry: {
+        client: path.resolve(__dirname, './app/client/client.tsx'),
+        server: path.resolve(__dirname, './app/server/server.ts')
+    },
     output: {
-        filename: 'client.js',
-        path: './dist/client',
+        filename: '[name].js',
+        path: './dist/',
         devtoolModuleFilenameTemplate: '[absolute-resource-path]',
         devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
     },
     resolve: {
         extensions: ['.ts', '.js', '.tsx']
-    },
-    devtool: 'source-map',
+    },   
     module: {
       rules: [
         { 
           test: /\.tsx?$/, 
           loader: 'ts-loader', 
           options: {
-            transpileOnly: true,
-            configFileName: './app/client/tsconfig.json',
+            configFileName: './app/tsconfig.json',
             logInfoToStdOut: true
           } 
         }
       ]
     },
     plugins: [
-      //new webpack.optimize.UglifyJsPlugin()
+          //new webpack.optimize.UglifyJsPlugin(),
+          // new Visualizer({
+          //   filename: './statistics.html'
+          // })        
     ]
 };
