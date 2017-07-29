4cd569505744f45d35e87c63aa3be5c35a30a7d3
wirejunky <antonie.coetzee@gmail.com>
2017-07-29 16:59:37 +0200

    getting testing to work

--- wallaby.js
+++ wallaby.js
@@ -2,16 +2,19 @@ module.exports = function (wallaby) {
   return {
     files: [
       'app/server/**/*.ts*',
+      'app/client/**/*.ts*',
       'app/server/**/*Test*',
-      {pattern: 'app/server/**/*.test.ts', ignore: true}
+      {pattern: 'app/server/**/*.test.ts', ignore: true},
+      {pattern: 'app/client/**/*.test.tsx', ignore: true}
     ],
 
     tests: [
-      'app/server/**/*.test.ts'
+      'app/server/**/*.test.ts',
+      'app/client/**/*.test.tsx'
     ],
 
     compilers: {
-      '**/*.ts': wallaby.compilers.typeScript({ module: 'commonjs' })
+      '**/*.ts*': wallaby.compilers.typeScript({ module: 'commonjs', jsx: 'react' })
     },
     testFramework: "jest",
     env: {
