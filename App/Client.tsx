53234e92c20f8450bd1a14f99601054b307da9a8
WireJunky <antonie.coetzee@gmail.com>
2017-08-10 21:02:30 +0200

    some relative path updates

--- App/Client.tsx
+++ App/Client.tsx
@@ -1,11 +1,11 @@
-import layer from "1.BaseLayoutAndRouting/Client/Layer"
+import layer from "./1.BaseLayoutAndRouting/Client/Layer"
 
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {BrowserRouter, Route} from 'react-router-dom'
 import {interfaces} from "inversify"
 
-import {IClientApplication, IClientApplicationKey} from "0.Bootstrap/Common/Application/IClientApplication"
+import {IClientApplication, IClientApplicationKey} from "./0.Bootstrap/Common/Application/IClientApplication"
 
 layer.Initialize();
 const Application = layer.container.get<interfaces.Newable<IClientApplication>>(IClientApplicationKey);
