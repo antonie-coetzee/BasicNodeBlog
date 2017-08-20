61f2b89fb899603feaf44c248fc03886f1d33de9
WireJunky <antonie.coetzee@gmail.com>
2017-08-20 13:45:12 +0200

    refactored webpack/npm build

---
+++ webpack.client.config.prod.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack'); 
+const devConfig = require('./webpack.client.config');
+const merge = require('webpack-merge');
+
+module.exports = merge(devConfig, { 
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+          minimize: true,
+          debug: false
+        }),
+        new webpack.DefinePlugin({
+          'process.env': {
+            'NODE_ENV': JSON.stringify('production')
+          }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          beautify: false,
+          mangle: {
+            screw_ie8: true,
+            keep_fnames: true
+          },
+          compress: {
+            screw_ie8: true
+          },
+          comments: false
+        })
+      ]
+});
